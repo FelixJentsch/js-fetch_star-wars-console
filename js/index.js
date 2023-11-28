96b81f94f27b58ddbdd8a4741873e06045d2b377
FelixJentsch <FelixJentsch91@googlemail.com>
2023-11-28 11:48:26 +0100

    challenge solved

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 console.clear();
 
-const url = "https://swapi.dev/api/people";
+const API_URL = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+
+  console.log(data);
+  console.log("Count: ", data.count);
+  console.log("Leia Organa's height: ", data.results[4].height);
+
+  const r2d2 = data.results.find((character) => character.name === "R2-D2");
+  console.log("R2-D2's eyecolor: ", r2d2.eye_color);
+  return data;
+}
 
 fetchData();
